2b8a2a60812143001fec54a2b229b462a478e2a6
Hallo <118673681+HALLOOO178@users.noreply.github.com>
2025-10-07 17:15:20 +0200

    Update libcandy.brawltvv47.js

--- libcandy.brawltvv47.js
+++ libcandy.brawltvv47.js
@@ -1,3 +1,6 @@
+// Script by hallo
+// https://dsc.gg/candybrawl
+
 const base = Process.getModuleByName('libg.so').base;
 
 Interceptor.attach(base.add(0xA57290), { // LogicBattleModeClient::addVisionUpdate
@@ -6,4 +9,5 @@ Interceptor.attach(base.add(0xA57290), { // LogicBattleModeClient::addVisionUpda
         args[1].add(104).writeInt(Spectators) // Spectators
         args[1].add(108).writeInt(1) // Is BrawlTV?(Set to 0 if you want normal spectators)
     }
+
 })
