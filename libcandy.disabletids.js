151bc07512e66896faa1874322d318e1365e120e
Hallo <118673681+HALLOOO178@users.noreply.github.com>
2025-10-16 17:03:47 +0200

    Add files via upload

---
+++ libcandy.disabletids.js
@@ -0,0 +1,25 @@
+// Disable TIDs in v47
+// Made by @hallo178
+// https://dsc.gg/candybrawl
+
+const base = Process.getModuleByName('libg.so').base;
+const malloc = new NativeFunction(Process.getModuleByName('libc.so').getExportByName('malloc'), 'pointer', ['uint']);
+
+const localizationGetString = base.add(0xD7FDB8);
+const stringCtor = new NativeFunction(base.add(0xE1F254), "pointer", ["pointer", "pointer"]);
+
+function createStringObject(txt) {
+    const sptr = Memory.allocUtf8String(txt);
+    const ptr = malloc(200);
+    stringCtor(ptr, sptr);
+    return ptr;
+}
+
+Interceptor.attach(localizationGetString, {
+    onEnter(args) {
+        this.tid = readStringObject(args[1])
+    },
+    onLeave(retval) {
+        retval.replace(createStringObject(this.tid))
+    }
+})
